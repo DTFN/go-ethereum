8e76b92bb8d9aad8b309f6361d6db48521941cf1
LaiZhibin <zhibin.lai@energy-blockchain.com>
2018-10-09 19:00:47 +0800

    debug

--- core/blacklist/blacklistdb.go
+++ core/blacklist/blacklistdb.go
@@ -7,10 +7,11 @@ import (
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"errors"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 var (
-	blacklistDBEntryExpiration int64           = 4
+	blacklistDBEntryExpiration int64           = 200
 	sendToLock                                 = "0x7777777777777777777777777777777777777777"
 	sendToUnlock                               = "0x8888888888888888888888888888888888888888"
 	w                          map[string]bool = make(map[string]bool)
@@ -28,11 +29,15 @@ func Lock(db *state.StateDB, address common.Address) {
 }
 
 func Unlock(db *state.StateDB, address common.Address, height *big.Int) {
-	db.SetState(address, lockInfoKey, common.BigToHash(height))
+	if db.GetState(address, lockInfoKey).Big().Int64() == -1 {
+		log.Info("unlock ...", address, height)
+		db.SetState(address, lockInfoKey, common.BigToHash(height))
+	}
 }
 
 func Validate(evm *vm.EVM, from common.Address, to *common.Address) error {
 	h := evm.StateDB.GetState(from, lockInfoKey).Big().Int64()
+	log.Info("validate ...", from, h)
 	locked := h == -1 || h+blacklistDBEntryExpiration >= evm.BlockNumber.Int64()
 	forbiddenSendee := to == nil || !w[to.Hex()]
 	if locked && forbiddenSendee {
