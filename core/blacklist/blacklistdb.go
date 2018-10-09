eb2956e408252508b970db1470ed768ad0644c90
LaiZhibin <zhibin.lai@energy-blockchain.com>
2018-10-09 20:36:12 +0800

    max_int64 as locked enum

--- core/blacklist/blacklistdb.go
+++ core/blacklist/blacklistdb.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 	"errors"
 	"fmt"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 var (
@@ -15,13 +16,16 @@ var (
 	sendToLock                                 = "0x7777777777777777777777777777777777777777"
 	sendToUnlock                               = "0x8888888888888888888888888888888888888888"
 	w                          map[string]bool = make(map[string]bool)
+	locked                                     = int64(^uint(0) >> 1) // max int64
 	lockInfoKey                                = common.BytesToHash([]byte("LOCK_INFO"))
-	lockInfoValue                              = common.BigToHash(big.NewInt(-1))
+	lockInfoValue                              = common.BigToHash(big.NewInt(locked))
 )
 
 func init() {
 	w[sendToLock] = true
 	w[sendToUnlock] = true
+	v, _ := rlp.EncodeToBytes(-1)
+	lockInfoValue = common.BytesToHash(v)
 }
 
 func Lock(db *state.StateDB, address common.Address) {
@@ -32,7 +36,7 @@ func Unlock(db *state.StateDB, address common.Address, height *big.Int) {
 	fmt.Println(address.Hex())
 	fmt.Println("height", height)
 	fmt.Println(db.GetState(address, lockInfoKey).Big().Int64())
-	if db.GetState(address, lockInfoKey).Big().Int64() == -1 {
+	if db.GetState(address, lockInfoKey) == lockInfoValue {
 		fmt.Println("unlock ...", address, height)
 		db.SetState(address, lockInfoKey, common.BigToHash(height))
 	}
@@ -41,7 +45,7 @@ func Unlock(db *state.StateDB, address common.Address, height *big.Int) {
 func Validate(evm *vm.EVM, from common.Address, to *common.Address) error {
 	h := evm.StateDB.GetState(from, lockInfoKey).Big().Int64()
 	if h != 0 {
-		locked := h == -1 || h+blacklistDBEntryExpiration >= evm.BlockNumber.Int64()
+		locked := h == locked || h+blacklistDBEntryExpiration >= evm.BlockNumber.Int64()
 		forbiddenSendee := to == nil || !w[to.Hex()]
 		if locked && forbiddenSendee {
 			return errors.New("locked")
--- core/blacklist/blacklistdb_test.go
+++ core/blacklist/blacklistdb_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	"math/big"
 )
 
 func init() {
@@ -15,4 +16,6 @@ func TestNewMemoryBlacklistDB(t *testing.T) {
 	fmt.Println(hashes)
 	fmt.Println(hashes.Big())
 	fmt.Println(hashes.Big().Int64())
+	u := int64(^uint(0) >> 1)
+	fmt.Println(common.BigToHash(big.NewInt(u)).Big().Int64())
 }
