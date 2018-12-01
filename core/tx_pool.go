9e1cb385615ab41671282e8820096453372c308e
Pippo Lai <3641721@qq.com>
2018-12-01 13:40:42 +0800

    add debug log

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -278,6 +278,13 @@ func (pool *TxPool) loop() {
 	// Track the previous head headers for transaction reorgs
 	head := pool.chain.CurrentBlock()
 
+	go func() {
+		for {
+			fmt.Println("eth.txPool.maxgas", pool.currentMaxGas)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
 	// Keep waiting for and reacting to the various events
 	for {
 		select {
