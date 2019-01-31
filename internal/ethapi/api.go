a6f877bd1efede53fd80e6148c70d93b05c9b63f
JamesRay <66258875@qq.com>
2019-01-31 17:07:13 +0800

    fix getReceipt 's  status

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1074,11 +1074,8 @@ func (s *PublicTransactionPoolAPI) GetTransactionReceipt(ctx context.Context, ha
 	// Assign receipt status or post state.
 	if len(receipt.PostState) > 0 {
 		fields["root"] = hexutil.Bytes(receipt.PostState)
-		fields["status"] = hexutil.Uint(types.ReceiptStatusSuccessful)
-	} else {
-		fields["status"] = hexutil.Uint(receipt.Status)
 	}
-
+	fields["status"] = hexutil.Uint(receipt.Status)
 	if receipt.Logs == nil {
 		fields["logs"] = [][]*types.Log{}
 	}
