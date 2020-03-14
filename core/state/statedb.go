fb6990b03f937ae0e1ec4177389a46fff7a071ad
JamesRay <66258875@qq.com>
2020-03-14 18:20:23 +0800

    fix init authTable

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -679,7 +679,7 @@ func (db *StateDB) InitAuthTable() (*txfilter.AuthTable) {
 	txfilter.CreateAuthTable()
 	if len(nextBytes) == 0 {
 		// no predata existed
-		panic("no pre AuthTable")
+		log.Error("no pre AuthTable")
 	} else {
 		log.Info("AuthTable Not nil")
 		err := json.Unmarshal(nextBytes, txfilter.EthAuthTable)
