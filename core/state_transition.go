6658fbc42c291d327c48f5d4e903d0149378b84e
JamesRay <66258875@qq.com>
2020-04-10 11:07:27 +0800

    upgrade ethereum 1

--- core/state_transition.go
+++ core/state_transition.go
@@ -134,7 +134,7 @@ func NewStateTransition(evm *vm.EVM, msg Message, gp *GasPool) *StateTransition 
 // the gas used (which includes gas refunds) and an error if it failed. An error always
 // indicates a core error meaning that the message would always fail for that particular
 // state and would never be accepted within a block.
-func ApplyMessage(evm *vm.EVM, msg Message, gp *GasPool, sim bool) ([]byte, uint64, bool, error) {
+func ApplyMessage(evm *vm.EVM, msg Message, gp *GasPool) ([]byte, uint64, bool, error) {
 	return NewStateTransition(evm, msg, gp).TransitionDb()
 }
 
