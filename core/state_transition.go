c3db25e36c0d2778ea6fc929f8b5a6adebda1e8b
webb.shi <bradyjoestar@gmail.com>
2020-05-18 15:07:55 +0800

    fix print way

--- core/state_transition.go
+++ core/state_transition.go
@@ -314,7 +314,7 @@ func (st *StateTransition) transitionDb(sim bool) (ret []byte, usedGas uint64, f
 
 	fmt.Printf("block.Height: %v,st.state.TrieHash in state transition: %X\n", st.evm.BlockNumber.Int64(), st.state.TrieHash())
 
-	fmt.Sprintf("0000..1 existed:%v\n",
+	fmt.Printf("0000..1 existed:%v\n",
 		st.state.Exist(common.HexToAddress("0x0000000000000000000000000000000000000001")))
 
 	return ret, st.gasUsed(), vmerr != nil, err
