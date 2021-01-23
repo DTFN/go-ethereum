09700a4faccb4871830fa271e9c69dd1d38b9d9e
james-ray <66258875@qq.com>
2021-01-23 14:57:01 +0800

    revert the code of eip158 compatible for production data.  code in gas and gasTable (missed revert code)

--- core/vm/gas.go
+++ core/vm/gas.go
@@ -34,8 +34,8 @@ const (
 //
 // The cost of gas was changed during the homestead price change HF.
 // As part of EIP 150 (TangerineWhistle), the returned gas is gas - base * 63 / 64.
-func callGas(isEip150 bool, availableGas, base uint64, callCost *uint256.Int) (uint64, error) {
-	if isEip150 {
+func callGas(isEip158 bool, availableGas, base uint64, callCost *uint256.Int) (uint64, error) {
+	if isEip158 {
 		availableGas = availableGas - base
 		gas := availableGas - availableGas/64
 		// If the bit length exceeds 64 bit we know that the newly calculated "gas" for EIP150
